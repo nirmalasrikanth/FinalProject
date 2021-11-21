34ef10646289a97460f66aa2d8171435df46d526
Nirmala <nnirmalasri@gmail.com>
2021-11-21 10:22:51 -0600

    changed the database settings

--- physicsprep/physicsprep/settings.py
+++ physicsprep/physicsprep/settings.py
@@ -78,10 +78,22 @@ WSGI_APPLICATION = 'physicsprep.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'finalproject',
+        'USER': 'finalprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
